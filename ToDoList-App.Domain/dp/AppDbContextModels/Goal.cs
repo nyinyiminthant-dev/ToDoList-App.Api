f6df3bf08cb3cb6c302f6549ae59f0a48eabeb04
nyinyiminthant-dev <nnyi37389@gmail.com>
2025-03-19 12:33:19 +0630

    Feat: Update database

---
+++ ToDoList-App.Domain/dp/AppDbContextModels/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList_App.Database.AppDbContextModels;
+
+public class Goal
+{
+    public int Id { get; set; }
+    public string Status { get; set; }
+    public DateTime DateTime { get; set; }
+    public int Duration { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int T_Id { get; set; }
+}
---
+++ ToDoList-App.Domain/dp/AppDbContextModels/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList_App.Database.AppDbContextModels;
+
+public class Task
+{
+    public int Id { get; set; }
+    public string TaskName { get; set; }
+    public DateTime dateTime { get; set; }
+    public string Staus { get; set; }
+    public string priority { get; set; }
+    public int C_Id { get; set; }
+}
