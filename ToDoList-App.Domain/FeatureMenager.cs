9c6c6f7e4e6d180ee3abdd7f614bee6be5e23772
nyinyiminthant-dev <nnyi37389@gmail.com>
2025-03-19 12:59:49 +0630

    Feat: Connection with di

--- ToDoList-App.Domain/FeatureMenager.cs
+++ ToDoList-App.Domain/FeatureMenager.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using ToDoList_App.Database.AppDbContextModels;
 
 namespace ToDoList_App.Domain;
 
 public static class FeatureMenager
 {
 
-    public static void AddToDoListFeatures (this WebApplicationBuilder builder)
+    public static void AddToDoListFeatures(this WebApplicationBuilder builder)
     {
-        builder.Services.AddDbContext<AppDbContext>();
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+        }, ServiceLifetime.Transient,
+        ServiceLifetime.Transient);
+
+
+
+        
     }
 
 }
